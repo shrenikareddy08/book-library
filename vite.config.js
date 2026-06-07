fa701ccc60d89143252ca0560dedc7acf3e76904
PUNREDDY SHRENIKA REDDY <punreddyshrenikareddy@PUNREDDYs-MacBook-Air.local>
2026-06-07 18:21:31 +0530

    fix base path for github + netlify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/book-library/",
-  plugins: [react()],
-  esbuild: {
-    jsx: "automatic",
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.js",
-  },
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/book-library/" : "/",
+    plugins: [react()],
+  };
 });
